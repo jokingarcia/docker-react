a5bef8d4bd5148919d2d6ee5d2d3ae56ce7d3e69
jokingarcia <jokingarcia75@hotmail.com>
2020-03-10 22:55:55 +0100

    changed app text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed on the Feature branch
+          learn react
         </a>
       </header>
     </div>
